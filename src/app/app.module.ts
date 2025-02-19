fa61c25ec24f6510886f38e80953a95085020506
wesleypds <wlypereiraa@gmail.com>
2025-02-18 23:22:26 -0300

    :bug: fix: ajustando erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule, 
-    IonicModule.forRoot(), 
+    IonicModule.forRoot(),
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
--- src/app/components/cadastro/cadastro.module.ts
+++ src/app/components/cadastro/cadastro.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,6 +12,7 @@ import { CadastroRoutingModule } from './cadastro-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     CadastroRoutingModule
   ],
   declarations: [CadastroComponent]
